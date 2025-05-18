2a503add36d692913534f288c3d375cf4829dc6e
godfam3 <demodae3@gmail.com>
2025-05-19 01:20:04 +0900

    feat: get_or_create_collection

--- BE/app/services/qdrant_service.py
+++ BE/app/services/qdrant_service.py
@@ -1,6 +1,7 @@
 from qdrant_client import AsyncQdrantClient
 from qdrant_client.models import Distance
 from core.config import settings
+from core.exception import CustomException, ExceptionCase
 
 
 class QdrantService:
@@ -19,6 +20,22 @@ class QdrantService:
         elif settings.DISTANCE_METRIC == "MANHATTAN":
             self.distance_metric = Distance.MANHATTAN
         else:
-            # 예외 처리
-            # raise ValueError("Invalid distance metric")
-            pass
+            raise CustomException(
+                ExceptionCase.INVALID_INPUT, detail="Invalid distance metric"
+            )
+
+    async def get_or_create_collection(self) -> None:
+        """시스템 시작 시 컬렉션 확인 및 생성"""
+        try:
+            if not await self.client.collection_exists(self.collection_name):
+                self.client.create_collection(
+                    collection_name=self.collection_name,
+                    vectors_config={
+                        "size": self.vector_size,
+                        "distance": self.distance_metric,
+                    },
+                )
+        except Exception as e:
+            raise CustomException(
+                exception_case=ExceptionCase.UNEXPECTED_ERROR, detail=str(e)
+            )
