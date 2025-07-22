7d14cd0d5ecda5e8d0a746cdbda29fc84b36c9a9
godfam3 <demodae3@gmail.com>
2025-07-22 17:48:03 +0900

    feat: langgraph 사용자 정의 state 모듈 추가

---
+++ BE/app/rag_graph/state.py
@@ -0,0 +1,25 @@
+"""
+Langgraph의 사용자 정의 state를 정의하는 모듈.
+"""
+
+from typing import Annotated, Sequence
+from typing_extensions import TypedDict
+from langchain_core.messages import BaseMessage
+from langgraph.graph.message import add_messages
+from langgraph.graph import StateGraph
+
+from schemas.schemas import Document
+
+
+class GraphState(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+    username: Annotated[str, "user"]
+    authority_group: Annotated[str, "authority_group"]
+    question: Annotated[str, "question"]
+    is_context_need: Annotated[bool, "is_context_need"]
+    context: Annotated[Sequence[Document], "context"]
+    old_context: Annotated[Sequence[Document], "old_context"]
+    answer: Annotated[str, "answer"]
+
+
+workflow = StateGraph(GraphState)
