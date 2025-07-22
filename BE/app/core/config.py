a1b1c8aa3840b532939a2f0424e28b5d19b97adc
godfam3 <demodae3@gmail.com>
2025-07-22 17:32:07 +0900

    feat: mcp config 추가

--- BE/app/core/config.py
+++ BE/app/core/config.py
@@ -1,5 +1,7 @@
 """RAG 서버 설정 파일"""
 
+import base64
+import json
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing import Literal, Optional
 
@@ -9,6 +11,7 @@ class Settings(BaseSettings):
 
     GEMINI_API_KEY: str
     NOTION_API_KEY: str
+    SMITHERY_API_KEY: str = "2a8e8fb9-a326-4f4d-adfa-62f9ae1febb8"
 
     QDRANT_SERVER: Optional[str] = "http://localhost:6333"
     QDRANT_COLLECTION_NAME: Optional[str] = "test_collection"
@@ -22,5 +25,30 @@ class Settings(BaseSettings):
 
 try:
     settings = Settings()
+except Exception as e:
+    raise RuntimeError("Fail to load environment vars") from e
+
+
+class MCPConfig:
+    """MCP 설정"""
+
+    smithery_api_key = settings.SMITHERY_API_KEY
+    notion_api_key = settings.NOTION_API_KEY
+
+    @classmethod
+    def get_mcp_config(cls, key: Literal["notion"]):
+        if key == "notion":
+            config = {"notionApiKey": cls.notion_api_key}
+            config_b64 = base64.b64encode(json.dumps(config).encode()).decode()
+            return {
+                "notion": {
+                    "url": f"https://server.smithery.ai/@smithery/notion/mcp?config={config_b64}&api_key={cls.smithery_api_key}",
+                    "transport": "streamable_http",
+                }
+            }
+
+
+try:
+    mcp_config = MCPConfig()
 except Exception as e:
     raise RuntimeError("Fail to load environment vars") from e
