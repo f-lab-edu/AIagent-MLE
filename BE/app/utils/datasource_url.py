4045a38fd47473a51d653f7bf914369e24a6f3c9
godfam3 <demodae3@gmail.com>
2025-08-24 01:35:31 +0900

    fix: DataSource type

--- BE/app/utils/datasource_url.py
+++ BE/app/utils/datasource_url.py
@@ -2,12 +2,12 @@
 데이터소스(ex. notion)의 url을 생성하거나 page_id를 추출하는 전처리 모듈
 """
 
-from typing import Literal
+from db.models import DataSource
 from core.exception import CustomException, ExceptionCase
 
 
-def context_url(datasource: Literal["notion"], page_id: str) -> str:
-    if datasource == "notion":
+def context_url(datasource: DataSource, page_id: str) -> str:
+    if datasource == DataSource.NOTION:
         return f"https://www.notion.so/{page_id}"
     else:
         raise CustomException(
@@ -15,6 +15,16 @@ def context_url(datasource: Literal["notion"], page_id: str) -> str:
         )
 
 
-def extract_page_id(url: str, datasource: Literal["notion"]) -> str:
-    if datasource == "notion":
-        return url.split("?")[0].split("/")[-1].split("-")[-1]
+def extract_page_id(url: str, datasource: DataSource) -> str:
+    try:
+        if datasource == DataSource.NOTION:
+            return url.split("?")[0].split("/")[-1].split("-")[-1]
+        else:
+            raise CustomException(
+                exception_case=ExceptionCase.INVALID_INPUT, detail="Invalid datasource"
+            )
+    except Exception as e:
+        raise CustomException(
+            exception_case=ExceptionCase.INVALID_INPUT,
+            detail=f"Unexpected url format.: {e}",
+        )
